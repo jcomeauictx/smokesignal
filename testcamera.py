99fd0326861b4e61c84ed1a35510d4e529d637d8
John-Clawd Comeau <jcclaw@clawd.unternet.net>
2026-02-18 05:02:04 +0100

    test camera

---
+++ testcamera.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+test read from camera
+'''
+import sys, cv2
+
+def test():
+    '''
+    open camera and read a single frame
+    '''
+    capture = cv2.VideoCapture(0)
+    if capture.isOpened():
+        captured = capture.read()
+        if captured[0]:
+            cv2.imshow(captured[1])
+            input()
+
+if __name__ == '__main__':
+    test()
