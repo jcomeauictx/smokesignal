f0fc205439aca0da852c372bf8c3f96c479ac87d
John Comeau <jc@unternet.net>
2026-02-17 22:01:13 -0700

    trying to figure out qrtools, docs are sparse

--- testdisplay.py
+++ testdisplay.py
@@ -3,7 +3,7 @@
 test display of QR code
 '''
 import sys, qrcode, logging
-from zbar import ImageScanner
+from qrtools import QR
 
 logging.basicConfig(level=logging.DEBUG if __debug__ else logging.INFO)
 
@@ -12,9 +12,11 @@ def test(text):
     display a QR code
     '''
     image = qrcode.make(text)
+    qrtools = QR(data=text)
+    logging.debug('qrtools: %s', qrtools)
     image.show()
-    barcode = ImageScanner.scan(image)
-    logging.info('barcode: %r', barcode)
+    code = qrtools.decode(image=image)
+    logging.info('qrtools: %s, code: %r', qrtools, code)
 
 if __name__ == '__main__':
     test(' '.join(sys.argv[1:]) or 'This is a test of smokesignal')
