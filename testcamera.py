0aadab5d84ddada122b913c33c42266400b9a499
John Comeau <jc@unternet.net>
2026-02-18 15:10:26 -0800

    now shows video but not frame

--- testcamera.py
+++ testcamera.py
@@ -9,11 +9,15 @@ def test():
     open camera and read a single frame
     '''
     capture = cv2.VideoCapture(0)
-    if capture.isOpened():
-        captured = capture.read()
-        if captured[0]:
-            cv2.imshow('frame captured', captured[1])
-            input()
+    while True:
+        if capture.isOpened():
+            captured = capture.read()
+            if captured[0]:
+                cv2.imshow('frame captured', captured[1])
+            if cv2.waitKey(1) & 0xff == ord('q'):
+                break
+    capture.release()
+    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     test()
