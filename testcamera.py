634918b0951dd38620c2b2e096714093b79ab7c6
John Comeau <jc@unternet.net>
2026-02-18 16:09:02 -0800

    a bit cleaner

--- testcamera.py
+++ testcamera.py
@@ -9,13 +9,12 @@ def test():
     open camera and read a single frame
     '''
     capture = cv2.VideoCapture(0)
-    while True:
-        if capture.isOpened():
-            captured = capture.read()
-            if captured[0]:
-                cv2.imshow('frame captured', captured[1])
-            if cv2.waitKey(1) & 0xff == ord('q'):
-                break
+    while capture.isOpened():
+        captured = capture.read()
+        if captured[0]:
+            cv2.imshow('frame captured', captured[1])
+        if cv2.waitKey(1) & 0xff == ord('q'):
+            break
     capture.release()
     cv2.destroyAllWindows()
 
