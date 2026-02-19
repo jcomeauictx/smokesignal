21b1f9851d224da6116c2b045c94d5d123ae3204
John Comeau <jc@unternet.net>
2026-02-18 19:14:12 -0800

    no errors but Tk window doesn't show

--- smokesignal.py
+++ smokesignal.py
@@ -15,10 +15,14 @@ def send(document):
     send document to peer
     '''
     capture = cv2.VideoCapture(0)
+    window = Tk()
+    label = Label(window, text='Starting...')
+    label.pack()
     with open(document) as data:
         chunked = chunks(data.read())
         while capture.isOpened():
-            show_qr(next(chunked))
+            chunk = next(chunked, None)
+            show_qr(label, chunk)
             captured = capture.read()
             if captured[0]:
                 cv2.imshow('frame captured', captured[1])
@@ -26,17 +30,21 @@ def send(document):
                 break
     capture.release()
     cv2.destroyAllWindows()
+    window.destroy()
 
-def show_qr(text):
+def show_qr(label, text):
     '''
     display a QR code
     '''
-    image = qrcode.make(text)
-    qrtools = QR(data=text)
-    logging.debug('qrtools: %s', vars(qrtools))
-    image.show()
-    code = qrtools.decode(image=image)
-    logging.info('qrtools: %s, code: %r', vars(qrtools), code)
+    if text:
+        image = qrcode.make(text)
+        photo = Photo(image)
+        qrtools = QR(data=text)
+        logging.debug('qrtools: %s', vars(qrtools))
+        label.configure(image=photo)
+        label.image = photo  # claude: necessary to thwart garbage collection
+        code = qrtools.decode(image=image)
+        logging.info('qrtools: %s, code: %r', vars(qrtools), code)
 
 def chunks(data, size=128):
     '''
